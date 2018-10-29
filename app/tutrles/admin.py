7bc495d32d7e4528694099f5a499dbbb5c484b5a
jacob <jacob.zhik@gmail.com>
2018-10-29 11:13:12 +0300

    add modules

---
+++ app/tutrles/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Turtle, Cards
+# Register your models here.
+
+
+admin.site.register(Turtle)
+admin.site.register(Cards)
---
+++ app/tutrles/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+# Create your models here.
+class Turtle(models.Model):
+    class Meta:
+        verbose_name = u'Черепаха'
+        verbose_name_plural = u'Черепахи'
+    COLORS = (
+        (1, 'Белая'),
+        (2, 'Синяя'),
+        (3, 'Фиолетовая'),
+        (4, 'Желтая'),
+        (5, 'Красная'),
+        (6, 'Зеленая'),
+    )
+    
+    name = models.CharField(max_length=200, verbose_name="Имя черепахи")
+    color = models.IntegerField(choices=COLORS,  default=1, verbose_name="Цвет")
+
+
+    def __str__(self):
+        return self.name
+
+class Cards(models.Model):
+    class Meta:
+        verbose_name = 'Карточка'
+        verbose_name_plural = 'Карточки'
+    COLORS = (
+        (1, 'Любая'),
+        (2, 'Синяя'),
+        (3, 'Фиолетовая'),
+        (4, 'Желтая'),
+        (5, 'Красная'),
+        (6, 'Зеленая'),
+        (7, 'Последняя'),
+    )
+    DIRACTION = (
+        (0, 'Назад'),
+        (1, 'Вперед'),
+    )
+    COUNTSTEP = (
+        (1, '1'),
+        (2, '2'),
+    )
+
+    name = models.CharField(max_length=200, verbose_name="Название")
+    color = models.IntegerField(choices=COLORS,  default=1, verbose_name="Цвет")
+    diraction = models.IntegerField(choices=DIRACTION,  default=0, verbose_name="Направление")
+    count_step = models.IntegerField(choices=COUNTSTEP,  default=1, verbose_name="Количество шагов")
+
+    def __str__(self):
+        return self.name
+ 
